2a06468219659327abe4613c3a528186319ddacc
LOPEZ RUBEN <rubenlopezrolando@gmail.com>
2026-03-24 03:15:50 -0300

    Fix Vercel ESM Zero Config

---
+++ api/index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from '../src/config/db.js';
+import { verifyAuth } from '../src/middlewares/authMiddleware.js';
+import courtRoutes from '../src/routes/courtRoutes.js';
+import userRoutes from '../src/routes/userRoutes.js';
+import reservationRoutes from '../src/routes/reservationRoutes.js';
+
+dotenv.config();
+
+// Inicializar BD
+connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Configuración de Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Montaje de Rutas de la API
+app.use('/api/courts', courtRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/reservations', reservationRoutes);
+
+// Ruta Base
+app.get('/', (req, res) => {
+  res.send('API de Gestión Canchas funcionando correctamente');
+});
+
+// Ruta Protegida de prueba
+app.get('/api/secure/dashboard', verifyAuth, (req, res) => {
+  res.json({
+    message: 'Esta información solo puede verla un usuario logueado',
+    user: req.user
+  });
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`🚀 Servidor ejecutándose en el puerto ${PORT}`);
+  });
+}
+
+export default app;
