0f5fdcd1c3c0e0a050da312c8d322859fa3d1cec
LOPEZ RUBEN <rubenlopezrolando@gmail.com>
2026-03-25 15:33:48 -0300

    fix: merge conflict CORS server.js

--- api/index.js
+++ api/index.js
@@ -20,6 +20,8 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
+  'https://clubestucuman.ar',
+  'https://www.clubestucuman.ar',
   String(process.env.FRONTEND_URL || '').replace(/\/$/, ''),
 ].filter(Boolean);
 
